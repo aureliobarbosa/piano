056b306a77ef7211caac0a5234169a70d54b9e4a
vinicius <viniciusribeirolara@gmail.com>
2025-06-11 13:56:21 -0300

    teclas/ frequencia

--- piano.py
+++ piano.py
@@ -15,6 +15,22 @@ while rodando:
     if QQ[0] == 'E':
         print("Sair")
         exit ()
+    elif QQ [0]=='Z':
+      print ('Frequencia = 220.0 Hz')
+    elif QQ [0]=='X':
+       print ('Frequencia = 493.88 Hz')
+    elif QQ [0]=='C':
+       print ('Frequencia = 261.63 Hz')
+    elif QQ [0]=='V':
+       print ('Frequencia = 293.66 Hz')
+    elif QQ [0]=='B':
+       print ('Frequencia = 329.63 Hz')
+    elif QQ [0]=='N':
+       print ('Frequencia = 349,228 Hz')
+    elif QQ [0]=='M':
+       print ('Frequencia = 392 Hz')
+
+
 
 
 

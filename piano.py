6e5aebb85bda11d8b5b386bf5929b2828c4c7612
vinicius <viniciusribeirolara@gmail.com>
2025-06-14 17:01:28 -0300

    codigo com som

--- piano.py
+++ piano.py
@@ -1,38 +1,52 @@
-print("""selecione alguma das teclas: 
-Z para la
-X para si
-C para do 
-V para re
-B para mi
-N para fa
-M para sol""")
- 
+import numpy as np
+import simpleaudio as sa
+
+
+def tocar_nota(frequencia, duracao=1, sample_rate=44100):
+   t = np.linspace(0, duracao, int(duracao * sample_rate), False)
+   nota = np.sin(frequencia * t * 2 * np.pi)
+   audio = nota * (2**15 - 1) / np.max(np.abs(nota))
+   audio = audio.astype(np.int16)
+   sa.play_buffer(audio, 1, 2, sample_rate)
+   
+
+teclas = {
+    'Z': 220.0,      # La
+    'X': 493.88,     # Si
+    'C': 261.63,     # Dó
+    'V': 293.66,     # Ré
+    'B': 329.63,     # Mi
+    'N': 349.23,     # Fá
+    'M': 392.0       # Sol
+}
+print("""Selecione alguma das teclas: 
+Z para La
+X para Si
+C para Dó 
+V para Ré
+B para Mi
+N para Fá
+M para Sol
+E para Sair
+""")
+
 rodando = True
 
-while rodando:
-    QQ = input("digite uma tecla: ")
-    print (QQ)
-    if QQ[0] == 'E':
-        print("Sair")
-        exit ()
-    elif QQ [0]=='Z':
-      print ('Frequencia = 220.0 Hz')
-    elif QQ [0]=='X':
-       print ('Frequencia = 493.88 Hz')
-    elif QQ [0]=='C':
-       print ('Frequencia = 261.63 Hz')
-    elif QQ [0]=='V':
-       print ('Frequencia = 293.66 Hz')
-    elif QQ [0]=='B':
-       print ('Frequencia = 329.63 Hz')
-    elif QQ [0]=='N':
-       print ('Frequencia = 349,228 Hz')
-    elif QQ [0]=='M':
-       print ('Frequencia = 392 Hz')
-    else:
-       print('tente novamente')
-       
+while rodando==True: 
+   tecla= input('Digite uma tecla: ').upper()
 
+   if tecla == 'E':
+      print('sair')
+      rodando=False
+   
+   elif tecla in teclas: 
+      frequencia= teclas[tecla]
+      print(f'frequencia = {frequencia}Hz')
+      tocar_nota(frequencia)
+      rodando=True
+   
+   else:
+       print('Tecla invalida. Tente novamente ')
 
 
 
